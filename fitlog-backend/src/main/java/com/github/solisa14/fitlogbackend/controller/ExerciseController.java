ca0ab501fef69efd9831ba4c714d868ca0347c2b
Alberto Solis Jr <solisa14@arizona.edu>
2025-06-13 14:10:35 -0700

    feat(controller): add detailed JavaDoc comments to ExerciseController
    Added comprehensive JavaDoc comments to the ExerciseController methods for improved clarity and documentation.

--- fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/controller/ExerciseController.java
+++ fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/controller/ExerciseController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-
+/**
+ * Controller for handling exercise-related operations using RESTful API endpoints.
+ */
 @RestController
 @RequestMapping("/api/v1/exercises")
 public class ExerciseController {
@@ -23,6 +25,11 @@ public class ExerciseController {
         this.exerciseService = exerciseService;
     }
 
+    /**
+     * Retrieves all exercises from the database.
+     *
+     * @return ResponseEntity containing a list of ExerciseResponseDto objects or a NO_CONTENT status if no exercises are found.
+     */
     @GetMapping
     public ResponseEntity<List<ExerciseResponseDto>> getAllExercises() {
         List<Exercise> foundExercises = exerciseService.getAllExercises();
@@ -33,6 +40,12 @@ public class ExerciseController {
         return ResponseEntity.status(HttpStatus.OK).body(exercises);
     }
 
+    /**
+     * Retrieves an exercise by its ID.
+     *
+     * @param id the ID of the exercise to retrieve
+     * @return ResponseEntity containing the ExerciseResponseDto if found, or NOT_FOUND status if not found
+     */
     @GetMapping("{id}")
     public ResponseEntity<ExerciseResponseDto> getExerciseById(@PathVariable Long id) {
         Optional<Exercise> exercise = exerciseService.getExerciseById(id);
@@ -41,12 +54,25 @@ public class ExerciseController {
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
+    /**
+     * Creates a new exercise.
+     *
+     * @param newExercise the ExerciseRequestDto containing the details of the new exercise
+     * @return ResponseEntity containing the created ExerciseResponseDto with status CREATED
+     */
     @PostMapping
     public ResponseEntity<ExerciseResponseDto> createExercise(@Valid @RequestBody ExerciseRequestDto newExercise) {
         Exercise savedExercise = exerciseService.saveExercise(newExercise.convertToExercise());
         return ResponseEntity.status(HttpStatus.CREATED).body(new ExerciseResponseDto(savedExercise));
     }
 
+    /**
+     * Updates an existing exercise by its ID.
+     *
+     * @param id                     the ID of the exercise to update
+     * @param updatedExerciseRequest the ExerciseRequestDto containing the updated details of the exercise
+     * @return ResponseEntity containing the updated ExerciseResponseDto if found, or NOT_FOUND status if not found
+     */
     @PutMapping("/{id}")
     public ResponseEntity<ExerciseResponseDto> updateExercise(@PathVariable Long id, @Valid @RequestBody ExerciseRequestDto updatedExerciseRequest) {
         Exercise updatedExercise = updatedExerciseRequest.convertToExercise();
@@ -56,6 +82,12 @@ public class ExerciseController {
                 .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
+    /**
+     * Deletes an exercise by its ID.
+     *
+     * @param id the ID of the exercise to delete
+     * @return ResponseEntity indicating the result of the deletion operation
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteExercise(@PathVariable Long id) {
         exerciseService.deleteExercise(id);
