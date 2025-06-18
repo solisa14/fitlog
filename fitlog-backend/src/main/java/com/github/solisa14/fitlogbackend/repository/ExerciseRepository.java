871f1b8d4665ac8b4a56c8cc4468a0009a7775ee
Alberto Solis Jr <solisa14@arizona.edu>
2025-06-18 16:57:52 -0700

    feat(exercise): add deleteByIdAndUser method to ExerciseRepository
    - Implemented a new method to delete an exercise by its ID and associated user, enhancing exercise management capabilities.

--- fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/repository/ExerciseRepository.java
+++ fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/repository/ExerciseRepository.java
@@ -1,12 +1,15 @@
 package com.github.solisa14.fitlogbackend.repository;
 
-import com.github.solisa14.fitlogbackend.model.Exercise;
-import com.github.solisa14.fitlogbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.solisa14.fitlogbackend.model.Exercise;
+import com.github.solisa14.fitlogbackend.model.User;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
@@ -14,5 +17,7 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
     List<Exercise> findAllByUser(User user);
 
+    @Modifying
+    @Transactional
     void deleteByIdAndUser(Long id, User user);
 }
