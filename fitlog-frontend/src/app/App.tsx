b8c594baec8f6689eda3e6257ceb3fdbe693dfcc
Alberto Solis Jr <solisa14@arizona.edu>
2025-06-17 17:56:35 -0700

    feat(routes): enhance routing and add new pages with styles
    - Wrapped the app with a styled container using `App.module.css` to improve layout and appearance.
    - Added new routes: `/dashboard`, `/workouts`, and `/profile` for enhanced functionality.

--- fitlog-frontend/src/app/App.tsx
+++ fitlog-frontend/src/app/App.tsx
@@ -3,27 +3,33 @@ import LoginPage from "../features/auth/login/LoginPage.tsx";
 import RegisterPage from "../features/auth/register/RegisterPage.tsx";
 import HomePage from "../features/home/HomePage.tsx";
 import PrivateRoutes from "./PrivateRoutes.tsx";
+import styles from './App.module.css';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                {/* Public Routes */}
-                <Route path="" element={<LoginPage/>}/>
-                <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/register"
-                       element={<RegisterPage/>}/>
+        <div className={styles.page}>
+            <BrowserRouter>
+                <Routes>
+                    {/* Public Routes */}
+                    <Route path="" element={<LoginPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/register"
+                           element={<RegisterPage/>}/>
 
-                {/* Private Routes */}
-                <Route element={<PrivateRoutes/>}>
-                    <Route path="/home" element={<HomePage/>}/>
-                </Route>
+                    {/* Private Routes */}
+                    <Route element={<PrivateRoutes/>}>
+                        <Route path="/home" element={<HomePage/>}/>
+                        <Route path="/dashboard" element={<HomePage/>}/>
+                        <Route path="/workouts" element={<p>Workouts Page</p>}/>
+                        <Route path="/profile" element={<p>Profile Page</p>}/>
+                    </Route>
 
-                <Route path="*" element={
-                    <p>There's nothing here: 404!</p>
-                }/>
-            </Routes>
-        </BrowserRouter>
+                    <Route path="*" element={
+                        <p>There's nothing here: 404!</p>
+                    }/>
+                </Routes>
+            </BrowserRouter>
+        </div>
     )
 }
 
