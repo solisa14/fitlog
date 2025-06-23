b051b0a710cf48b04cd4834015f7a7bcbda20bd5
Alberto Solis Jr <solisa14@arizona.edu>
2025-06-23 08:42:23 -0700

    feat(exercise): add muscle group and tracking type in exercise management

--- fitlog-frontend/src/features/exercise/ExercisePage.tsx
+++ fitlog-frontend/src/features/exercise/ExercisePage.tsx
@@ -1,101 +1,122 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import {
-  createExercise,
-  deleteExercise,
-  getExercises,
-  updateExercise,
+    createExercise,
+    deleteExercise,
+    getExercises,
+    updateExercise,
 } from "../../services/exercise-service";
 import ExerciseTable from "./ExerciseTable";
 import ExerciseForm from "./ExerciseForm";
 import styles from "./Exercise.module.css";
 
 export interface Exercise {
-  id: string;
-  name: string;
-  description: string;
+    id: number | string;
+    name: string;
+    muscleGroups: MuscleGroup[];
+    trackingType: TrackingType;
 }
 
+export type MuscleGroup =
+    | "CHEST"
+    | "BACK"
+    | "SHOULDERS"
+    | "ARMS"
+    | "LEGS"
+    | "CORE"
+    | "GLUTES"
+    | "FULL_BODY";
+
+export type TrackingType =
+    | "REPS_AND_WEIGHT"
+    | "TIME_BASED"
+    | "REPS_ONLY"
+    | "DISTANCE_AND_DURATION";
+
 export default function ExercisePage() {
-  const [exercises, setExercises] = useState<Exercise[]>([]);
-  const [exerciseToEdit, setExerciseToEdit] = useState<Exercise | null>(null);
-  const [displayExerciseForm, setDisplayExerciseForm] = useState(false);
+    const [exercises, setExercises] = useState<Exercise[]>([]);
+    const [exerciseToEdit, setExerciseToEdit] = useState<Exercise | null>(null);
+    const [displayExerciseForm, setDisplayExerciseForm] = useState(false);
 
-  async function handleCreateExercise(exercise: Exercise) {
-    setDisplayExerciseForm(false);
-    const createdExercise = await createExercise({
-      id: "",
-      name: exercise.name,
-      description: exercise.description,
-    });
-    setExercises([...exercises, createdExercise]);
-  }
+    async function handleCreateExercise(exercise: Exercise) {
+        setDisplayExerciseForm(false);
+        const createdExercise = await createExercise({
+            id: "",
+            name: exercise.name,
+            muscleGroups: exercise.muscleGroups,
+            trackingType: exercise.trackingType
+        });
+        setExercises([...exercises, createdExercise]);
+    }
 
-  async function fetchExercises() {
-    const fetchedExercises = await getExercises();
-    setExercises(fetchedExercises);
-  }
+    async function fetchExercises() {
+        const fetchedExercises = await getExercises();
+        setExercises(fetchedExercises);
+    }
 
-  async function handleUpdateExercise(exercise: Exercise) {
-    setDisplayExerciseForm(false);
-    const updatedExercise = await updateExercise({
-      id: exercise.id,
-      name: exercise.name,
-      description: exercise.description,
-    });
-    setExercises(
-      exercises.map((e) => (e.id === exercise.id ? updatedExercise : e))
-    );
-  }
+    async function handleUpdateExercise(exercise: Exercise) {
+        setDisplayExerciseForm(false);
+        const updatedExercise = await updateExercise({
+            id: "",
+            name: exercise.name,
+            muscleGroups: exercise.muscleGroups,
+            trackingType: exercise.trackingType
+        });
+        setExercises(
+            exercises.map((e) => (e.id === exercise.id ? updatedExercise : e))
+        );
+    }
 
-  async function handleDeleteExercise(id: string) {
-    await deleteExercise(id);
-    setExercises(exercises.filter((e) => e.id !== id));
-  }
+    async function handleDeleteExercise(id: string) {
+        await deleteExercise(id);
+        setExercises(exercises.filter((e) => e.id !== id));
+    }
 
-  function handleEditExercise(exercise: Exercise) {
-    setExerciseToEdit(exercise);
-    setDisplayExerciseForm(true);
-  }
+    function handleEditExercise(exercise: Exercise) {
+        setExerciseToEdit(exercise);
+        setDisplayExerciseForm(true);
+    }
 
-  function handleToggleExerciseForm() {
-    setDisplayExerciseForm(!displayExerciseForm);
-    setExerciseToEdit(null);
-  }
+    function handleToggleExerciseForm() {
+        setDisplayExerciseForm(!displayExerciseForm);
+        setExerciseToEdit(null);
+    }
 
-  useEffect(() => {
-    fetchExercises();
-  }, []);
+    useEffect(() => {
+        fetchExercises().catch((error) => {
+            console.log(`Failed to fetch exercises ${error}`);
+        });
+    }, []);
 
-  return (
-    <div className={styles.exercisePageContainer}>
-      <div className={styles.exercisePageContent}>
-        <div className={styles.exercisePageHeader}>
-          <h1>My Exercises</h1>
-          <button
-            className={styles.createButton}
-            onClick={handleToggleExerciseForm}
-          >
-            + Create Exercise
-          </button>
-        </div>
-        <ExerciseTable
-          exercises={exercises}
-          onEdit={handleEditExercise}
-          onDelete={handleDeleteExercise}
-        />
-        {displayExerciseForm && (
-          <div className={styles.formOverlay}>
-            <div className={styles.formContainer}>
-              <ExerciseForm
-                exercise={exerciseToEdit}
-                onCancel={handleToggleExerciseForm}
-                onCreate={handleCreateExercise}
-                onEdit={handleUpdateExercise}
-              />
+    return (
+        <div className={styles.exercisePageContainer}>
+            <div className={styles.exercisePageContent}>
+                <div className={styles.exercisePageHeader}>
+                    <h1>My Exercises</h1>
+                    <button
+                        className={styles.createButton}
+                        onClick={handleToggleExerciseForm}
+                    >
+                        + Create Exercise
+                    </button>
+                </div>
+                <ExerciseTable
+                    exercises={exercises}
+                    onEdit={handleEditExercise}
+                    onDelete={handleDeleteExercise}
+                />
+                {displayExerciseForm && (
+                    <div className={styles.formOverlay}>
+                        <div className={styles.formContainer}>
+                            <ExerciseForm
+                                exercise={exerciseToEdit}
+                                onCancel={handleToggleExerciseForm}
+                                onCreate={handleCreateExercise}
+                                onEdit={handleUpdateExercise}
+                            />
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- fitlog-frontend/src/services/exercise-service.ts
+++ fitlog-frontend/src/services/exercise-service.ts
@@ -1,99 +1,105 @@
-import { getAuthToken } from "./auth-service.ts";
+import {getAuthToken} from "./auth-service.ts";
+import type {
+    MuscleGroup,
+    TrackingType
+} from "../features/exercise/ExercisePage.tsx";
 
 export interface ExerciseRequest {
-  id: string;
-  name: string;
-  description: string;
+    id: number | string;
+    name: string;
+    muscleGroups: MuscleGroup[];
+    trackingType: TrackingType;
 }
 
 export interface ExerciseResponse {
-  id: string;
-  name: string;
-  description: string;
+    id: number | string;
+    name: string;
+    muscleGroups: MuscleGroup[];
+    trackingType: TrackingType;
 }
 
 const BASE_URL: string = "http://localhost:8080/api/v1/exercises";
 
 export async function createExercise(
-  exerciseRequest: ExerciseRequest
+    exerciseRequest: ExerciseRequest
 ): Promise<ExerciseResponse> {
-  const token = getAuthTokenAndValidate();
+    const token = getAuthTokenAndValidate();
 
-  const response = await fetch(BASE_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(exerciseRequest),
-  });
+    const response = await fetch(BASE_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(exerciseRequest),
+    });
 
-  if (!response.ok) {
-    throw new Error("Failed to create exercise");
-  }
+    if (!response.ok) {
+        throw new Error("Failed to create exercise");
+    }
 
-  return await response.json();
+    return await response.json();
 }
 
 export async function getExercises(): Promise<ExerciseResponse[]> {
-  const token = getAuthTokenAndValidate();
+    const token = getAuthTokenAndValidate();
 
-  const response = await fetch(BASE_URL, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const response = await fetch(BASE_URL, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    });
 
-  if (!response.ok) {
-    throw new Error("Failed to get exercises");
-  }
+    if (!response.ok) {
+        throw new Error("Failed to get exercises");
+    }
 
-  return await response.json();
+    return await response.json();
 }
 
 export async function updateExercise(
-  exerciseRequest: ExerciseRequest
+    exerciseRequest: ExerciseRequest
 ): Promise<ExerciseResponse> {
-  const token = getAuthTokenAndValidate();
+    const token = getAuthTokenAndValidate();
 
-  const response = await fetch(`${BASE_URL}/${exerciseRequest.id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(exerciseRequest),
-  });
+    const response = await fetch(`${BASE_URL}/${exerciseRequest.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(exerciseRequest),
+    });
 
-  if (!response.ok) {
-    console.log(response);
-    throw new Error("Failed to update exercise");
-  }
+    if (!response.ok) {
+        console.log(response);
+        throw new Error("Failed to update exercise");
+    }
 
-  return await response.json();
+    return await response.json();
 }
 
 export async function deleteExercise(id: string) {
-  const token = getAuthTokenAndValidate();
+    const token = getAuthTokenAndValidate();
 
-  const response = await fetch(`${BASE_URL}/${id}`, {
-    method: "DELETE",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+    const response = await fetch(`${BASE_URL}/${id}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
 
-  if (!response.ok) {
-    throw new Error("Failed to delete exercise");
-  }
+    if (!response.ok) {
+        throw new Error("Failed to delete exercise");
+    }
 }
 
 function getAuthTokenAndValidate(): string {
-  const token = getAuthToken();
-  if (!token) {
-    throw new Error("No auth token found");
-  }
-  return token;
+    const token = getAuthToken();
+    if (!token) {
+        throw new Error("No auth token found");
+    }
+    return token;
 }
