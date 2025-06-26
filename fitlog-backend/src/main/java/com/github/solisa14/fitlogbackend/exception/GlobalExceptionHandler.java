5dc807249facfa76a27e90f434d8f946e42897e4
Alberto Solis Jr <solisa14@arizona.edu>
2025-06-26 08:25:19 -0700

    feat(exception): implement custom error response for exception handling
    Added `ErrorResponse` class to standardize API error responses. Updated `GlobalExceptionHandler` to utilize `ErrorResponse` instead of `ProblemDetail`, improving the consistency and clarity of error messages.

--- fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/exception/GlobalExceptionHandler.java
+++ fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.github.solisa14.fitlogbackend.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -27,122 +28,149 @@ public class GlobalExceptionHandler {
      * Handles MethodArgumentNotValidException, typically thrown when validation on an argument
      * annotated with @Valid fails.
      *
-     * @param e The MethodArgumentNotValidException instance.
-     * @return A ResponseEntity containing ProblemDetail with BAD_REQUEST status and error details.
+     * @param e       The MethodArgumentNotValidException instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with BAD_REQUEST status and error details.
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e, HttpServletRequest request) {
         List<FieldError> errors = e.getBindingResult().getFieldErrors();
         // Extracts field errors and formats them into a list of strings
         List<String> errorMessages = errors.stream()
                 .map(error -> error.getField() + ": " + error.getDefaultMessage()).toList();
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST.value());
-        problemDetail.setTitle("Bad Request");
-        problemDetail.setDetail(errorMessages.toString()); // Provides detailed validation error
-        // messages
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                errorMessages.toString(),
+                request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
     /**
      * Handles EmailAlreadyExistsException, a custom exception thrown when a user tries to register
      * with an email that already exists in the database.
      *
-     * @param e The EmailAlreadyExistsException instance.
-     * @return A ResponseEntity containing ProblemDetail with CONFLICT status and error details.
+     * @param e       The EmailAlreadyExistsException instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with CONFLICT status and error details.
      */
     @ExceptionHandler(com.github.solisa14.fitlogbackend.exception.EmailAlreadyExistsException.class)
-    public ResponseEntity<ProblemDetail> handleEmailAlreadyExistsException(
-            com.github.solisa14.fitlogbackend.exception.EmailAlreadyExistsException e) { // Changed
-        // parameter
-        // type
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT.value());
-        problemDetail.setTitle("Email Already Exists");
-        problemDetail.setDetail(e.getMessage()); // Uses the message from the custom exception
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(
+            com.github.solisa14.fitlogbackend.exception.EmailAlreadyExistsException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
     /**
      * Handles ResourceNotFoundException, a custom exception thrown when a requested resource is not
      * found in the system.
      *
-     * @param e The ResourceNotFoundException instance.
-     * @return A ResponseEntity containing ProblemDetail with NOT_FOUND status and error details.
+     * @param e       The ResourceNotFoundException instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with NOT_FOUND status and error details.
      */
     @ExceptionHandler(com.github.solisa14.fitlogbackend.exception.ResourceNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleResourceNotFoundException(
-            com.github.solisa14.fitlogbackend.exception.ResourceNotFoundException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND.value());
-        problemDetail.setTitle("Resource Not Found");
-        problemDetail.setDetail(e.getMessage()); // Uses the message from the custom exception
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
+            com.github.solisa14.fitlogbackend.exception.ResourceNotFoundException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     /**
      * Handles UsernameNotFoundException, thrown by Spring Security when a user is not found during
      * the authentication process.
      *
-     * @param e The UsernameNotFoundException instance.
-     * @return A ResponseEntity containing ProblemDetail with NOT_FOUND status and error details.
+     * @param e       The UsernameNotFoundException instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with NOT_FOUND status and error details.
      */
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleUsernameNotFoundException(
-            UsernameNotFoundException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND.value());
-        problemDetail.setTitle("User Not Found");
-        problemDetail.setDetail(e.getMessage()); // Uses the message from the Spring Security
-        // exception
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
+    public ResponseEntity<ErrorResponse> handleUsernameNotFoundException(
+            UsernameNotFoundException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     /**
      * Handles BadCredentialsException, thrown by Spring Security when authentication fails due to
      * incorrect credentials.
      *
-     * @param e The BadCredentialsException instance.
-     * @return A ResponseEntity containing ProblemDetail with UNAUTHORIZED status and error details.
+     * @param e       The BadCredentialsException instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with UNAUTHORIZED status and error details.
      */
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ProblemDetail> handleBadCredentialsException(BadCredentialsException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED.value());
-        problemDetail.setTitle("Invalid Credentials");
-        problemDetail.setDetail("Email or password is incorrect"); // Provides a generic message for
-        // security reasons
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(problemDetail);
+    public ResponseEntity<ErrorResponse> handleBadCredentialsException(
+            BadCredentialsException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                "Email or password is incorrect",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
     /**
      * Handles AccessDeniedException, thrown by Spring Security when an authenticated user attempts
      * to access a resource they are not authorized to access.
      *
-     * @param e The AccessDeniedException instance.
-     * @return A ResponseEntity containing ProblemDetail with FORBIDDEN status and error details.
+     * @param e       The AccessDeniedException instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with FORBIDDEN status and error details.
      */
     @ExceptionHandler(AccessDeniedException.class)
-    public ResponseEntity<ProblemDetail> handleAccessDeniedException(AccessDeniedException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.FORBIDDEN.value());
-        problemDetail.setTitle("Forbidden");
-        problemDetail.setDetail("You do not have permission to access this resource."); // Provides
-        // a clear
-        // message
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(problemDetail);
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(
+            AccessDeniedException e, HttpServletRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.FORBIDDEN.value(),
+                HttpStatus.FORBIDDEN.getReasonPhrase(),
+                "You do not have permission to access this resource.",
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorResponse);
     }
 
     /**
      * Handles all other unhandled exceptions, serving as a global fallback. It's advisable to log
      * the exception here for debugging purposes.
      *
-     * @param e The Exception instance.
-     * @return A ResponseEntity containing ProblemDetail with INTERNAL_SERVER_ERROR status.
+     * @param e       The Exception instance.
+     * @param request The HttpServletRequest that resulted in the exception.
+     * @return A ResponseEntity containing ErrorResponse with INTERNAL_SERVER_ERROR status.
      */
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ProblemDetail> handleGlobalException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception e, HttpServletRequest request) {
         log.error("Unhandled exception occurred: ", e);
-        ProblemDetail problemDetail =
-                ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-        problemDetail.setTitle("Internal Server Error");
-        // Provide a generic message to the client for security and user experience
-        problemDetail.setDetail("An unexpected error occurred. Please try again later.");
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetail);
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 }
