b3467a793044e599ef56cc95975c92b4be41c2e1
Alberto Solis Jr <solisa14@arizona.edu>
2025-07-01 11:17:12 -0700

    refactor(enums): enhance MuscleGroup and TrackingType enums with string representations and JSON serialization
    Updated the MuscleGroup and TrackingType enums to include string representations for each value, improving clarity and usability. Added JSON serialization methods to facilitate integration with external systems. This refactor enhances the maintainability and readability of the codebase.

--- fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/enums/MuscleGroup.java
+++ fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/enums/MuscleGroup.java
@@ -1,14 +1,70 @@
 package com.github.solisa14.fitlogbackend.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum MuscleGroup {
-    CHEST,
-    BACK,
-    SHOULDERS,
-    ARMS,
-    LEGS,
-    CORE,
-    GLUTES,
-    FULL_BODY;
+    // Push
+    CHEST("chest"),
+    SHOULDERS("shoulders"),
+    TRICEPS("triceps"),
+    
+    // Pull
+    BACK("back"),
+    LATS("lats"),
+    BICEPS("biceps"),
+    REAR_DELTS("rear_delts"),
+    
+    // Core
+    ABS("abs"),
+    OBLIQUES("obliques"),
+    LOWER_BACK("lower_back"),
+    
+    // Lower
+    QUADRICEPS("quadriceps"),
+    HAMSTRINGS("hamstrings"),
+    GLUTES("glutes"),
+    CALVES("calves"),
+    HIP_FLEXORS("hip_flexors"),
+    
+    // Compound/Full Body
+    FULL_BODY("full_body"),
+    
+    // Specific Areas
+    FOREARMS("forearms"),
+    TRAPS("traps"),
+    NECK("neck"),
+    CARDIO("cardio"),
+    
+    // Functional Groups
+    HIP_ABDUCTORS("hip_abductors"),
+    HIP_ADDUCTORS("hip_adductors"),
+    ROTATOR_CUFF("rotator_cuff");
+
+    private final String name;
+
+    MuscleGroup(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonValue
+    public String toJson() {
+        return this.name;
+    }
+
+    @JsonCreator
+    public static MuscleGroup fromJson(String name) {
+        for (MuscleGroup muscleGroup : MuscleGroup.values()) {
+            if (muscleGroup.name.equals(name)) {
+                return muscleGroup;
+            }
+        }
+        throw new IllegalArgumentException("Invalid muscle group: " + name);
+    }
 
     @Override
     public String toString() {
