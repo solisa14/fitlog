cc898e96702458dc093f295deefc960165d2cb71
Alberto Solis Jr <solisa14@arizona.edu>
2025-07-02 16:29:56 -0700

    style(AuthForm): refine layout and input styling with Tailwind CSS
    Updated the AuthForm component to enhance the layout and input field styles using Tailwind CSS. Adjusted class names for better consistency and responsiveness, improving the overall user experience.

--- fitlog-frontend/src/components/AuthForm.tsx
+++ fitlog-frontend/src/components/AuthForm.tsx
@@ -46,16 +46,16 @@ export default function AuthForm({
   };
 
   return (
-    <div className="flex flex-col bg-white dark:bg-gray-900 items-center justify-center min-h-screen transition-colors duration-300">
-      <h1 className="text-5xl font-bold mb-10 text-gray-900 dark:text-white transition-colors duration-300">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-white transition-colors duration-300 dark:bg-gray-900">
+      <h1 className="mb-10 text-5xl font-bold text-gray-900 transition-colors duration-300 dark:text-white">
         {title}
       </h1>
       <form
-        className="flex flex-col gap-4 mb-5 w-full max-w-md px-4"
+        className="flex flex-col gap-4 px-4 mb-5 w-full max-w-md"
         onSubmit={handleSubmit}
       >
         <input
-          className="bg-gray-200 dark:bg-gray-800 placeholder:text-gray-500 dark:placeholder:text-gray-400 border-2 border-gray-500 dark:border-gray-400 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:focus:ring-gray-300 text-gray-900 dark:text-white transition-colors duration-200"
+          className="p-2 text-gray-900 bg-gray-200 rounded-xl border-2 border-gray-500 transition-colors duration-200 dark:bg-gray-800 placeholder:text-gray-500 dark:placeholder:text-gray-400 dark:border-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:focus:ring-gray-300 dark:text-white"
           type="email"
           name="email"
           placeholder="Email"
@@ -64,7 +64,7 @@ export default function AuthForm({
           onChange={handleChange}
         />
         <input
-          className="bg-gray-200 dark:bg-gray-800 placeholder:text-gray-500 dark:placeholder:text-gray-400 border-2 border-gray-500 dark:border-gray-400 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:focus:ring-gray-300 text-gray-900 dark:text-white transition-colors duration-200"
+          className="p-2 text-gray-900 bg-gray-200 rounded-xl border-2 border-gray-500 transition-colors duration-200 dark:bg-gray-800 placeholder:text-gray-500 dark:placeholder:text-gray-400 dark:border-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:focus:ring-gray-300 dark:text-white"
           type="password"
           name="password"
           placeholder="Password"
@@ -74,14 +74,14 @@ export default function AuthForm({
         />
         {errorMessage && <ErrorMessage message={errorMessage} />}
         <button
-          className="bg-red-500 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-900 text-white rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-red-300 dark:focus:ring-red-800 transition-colors duration-200"
+          className="p-2 text-white bg-red-500 rounded-full transition-colors duration-200 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-300 dark:focus:ring-red-800"
           type="submit"
         >
           {buttonText}
         </button>
       </form>
       <Link
-        className="text-red-500 hover:text-red-800 dark:text-red-400 dark:hover:text-red-200 hover:underline transition-colors duration-200"
+        className="text-red-500 transition-colors duration-200 hover:text-red-800 dark:text-red-400 dark:hover:text-red-200 hover:underline"
         to={linkTo}
       >
         {linkText}
