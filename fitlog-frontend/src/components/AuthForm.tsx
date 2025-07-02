7a2f954b870ff7161f1c23779fd5029bc4c937fa
Alberto Solis Jr <solisa14@arizona.edu>
2025-07-02 09:33:15 -0700

    style(AuthForm, ErrorMessage): enhance UI with Tailwind CSS classes
    Updated the AuthForm and ErrorMessage components to improve styling using Tailwind CSS. Added responsive classes for layout, input fields, buttons, and error messages, enhancing the overall user experience and visual appeal.

--- fitlog-frontend/src/components/AuthForm.tsx
+++ fitlog-frontend/src/components/AuthForm.tsx
@@ -46,10 +46,16 @@ export default function AuthForm({
   };
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <form onSubmit={handleSubmit}>
+    <div className="flex flex-col bg-white dark:bg-gray-900 items-center justify-center min-h-screen transition-colors duration-300">
+      <h1 className="text-5xl font-bold mb-10 text-gray-900 dark:text-white transition-colors duration-300">
+        {title}
+      </h1>
+      <form
+        className="flex flex-col gap-4 mb-5 w-full max-w-md px-4"
+        onSubmit={handleSubmit}
+      >
         <input
+          className="bg-gray-200 dark:bg-gray-800 placeholder:text-gray-500 dark:placeholder:text-gray-400 border-2 border-gray-500 dark:border-gray-400 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:focus:ring-gray-300 text-gray-900 dark:text-white transition-colors duration-200"
           type="email"
           name="email"
           placeholder="Email"
@@ -58,6 +64,7 @@ export default function AuthForm({
           onChange={handleChange}
         />
         <input
+          className="bg-gray-200 dark:bg-gray-800 placeholder:text-gray-500 dark:placeholder:text-gray-400 border-2 border-gray-500 dark:border-gray-400 rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-gray-700 dark:focus:ring-gray-300 text-gray-900 dark:text-white transition-colors duration-200"
           type="password"
           name="password"
           placeholder="Password"
@@ -66,9 +73,19 @@ export default function AuthForm({
           onChange={handleChange}
         />
         {errorMessage && <ErrorMessage message={errorMessage} />}
-        <button type="submit">{buttonText}</button>
+        <button
+          className="bg-red-500 hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-900 text-white rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-red-300 dark:focus:ring-red-800 transition-colors duration-200"
+          type="submit"
+        >
+          {buttonText}
+        </button>
       </form>
-      <Link to={linkTo}>{linkText}</Link>
+      <Link
+        className="text-red-500 hover:text-red-800 dark:text-red-400 dark:hover:text-red-200 hover:underline transition-colors duration-200"
+        to={linkTo}
+      >
+        {linkText}
+      </Link>
     </div>
   );
 }
