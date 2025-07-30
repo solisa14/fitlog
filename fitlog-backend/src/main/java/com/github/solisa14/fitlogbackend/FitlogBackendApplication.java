8c1df4fdf91c10d4c6e4c2709cf0a4c0dedbb3ba
Alberto Solis Jr <solisa14@arizona.edu>
2025-07-30 09:48:28 -0700

    Remove outdated TODO comments in FitlogBackendApplication.

--- fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/FitlogBackendApplication.java
+++ fitlog-backend/src/main/java/com/github/solisa14/fitlogbackend/FitlogBackendApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FitlogBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FitlogBackendApplication.class, args);
-    }
-
-    // TODO: remove loggedAt from workout
-    // TODO: implement the frontend for the working workouts backend endpoints
+  public static void main(String[] args) {
+    SpringApplication.run(FitlogBackendApplication.class, args);
+  }
 }
